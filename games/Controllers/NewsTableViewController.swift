3a5a6d38912df51e9f7928393a5bcd3ce89c0ceb
Maxim Skorynin <skoryninmaksim1@gmail.com>
2018-11-27 18:41:14 +0500

    Commit: - Changed category of app

---
+++ games/Controllers/NewsTableViewController.swift
@@ -0,0 +1,79 @@
+//
+//  NewsTableViewController.swift
+//  ecology
+//
+//  Created by Maxim Skorynin on 16/11/2018.
+//  Copyright © 2018 Maxim Skorynin. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    let identCell = "NewsCell"
+    var allNews : [News] = []
+    
+    var refresher : UIRefreshControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.layer.cornerRadius = 4
+        
+        let nib = UINib.init(nibName: identCell, bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: identCell)
+        
+        allNews = ParseNews.globalNews
+        
+        refresher = UIRefreshControl()
+        refresher.tintColor = UIColor.white.withAlphaComponent(0.8)
+        refresher.addTarget(self, action: #selector(self.refresh), for: UIControl.Event.valueChanged)
+        self.tableView.insertSubview(refresher!, at: 0)
+        
+    }
+    
+    @objc func refresh() {
+        self.navigationItem.title = "Обновление..."
+        ParseNews.parse() { isUpdate in
+            if isUpdate {
+                self.allNews = ParseNews.globalNews
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                    self.navigationItem.title = "Новости игровой индустрии"
+                    if self.refresher.isRefreshing {
+                        self.refresher.endRefreshing()
+                    }
+                }
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let news = allNews[indexPath.row] as News? {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: identCell) as? NewsTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.configure(news: news)
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allNews.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let detailedNewsVC = self.storyboard?.instantiateViewController(withIdentifier: Vc.detailedNewsViewController) as? DetailedNewsViewController else {return}
+        
+        detailedNewsVC.news = allNews[indexPath.row]
+        self.navigationController?.pushViewController(detailedNewsVC, animated: true)
+    }
+}
---
+++ games/Network/ParseNews.swift
@@ -0,0 +1,72 @@
+//
+//  ParseNews.swift
+//  ecology
+//
+//  Created by Maxim Skorynin on 16/11/2018.
+//  Copyright © 2018 Maxim Skorynin. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SWXMLHash
+
+class ParseNews {
+    
+    static var globalNews : [News] = []
+    
+    static func formatTime(dateString: String) -> Int {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat="dd.MM.yyyy HH:mm"
+        if let date = dateFormatter.date(from: dateString) {
+            let timestamp:Int = Int(date.timeIntervalSince1970)
+            return timestamp
+        }
+        return Int(Date().timeIntervalSinceNow)
+    }
+    
+    public static func parse(completionHandler: @escaping (Bool) -> ()) {
+        
+        if globalNews.count > 0 {
+            globalNews.removeAll()
+        }
+        
+        Alamofire.request(Constants.url, method: .get).response(completionHandler: { response in
+            if response.error == nil && response.data != nil {
+                if let xml = SWXMLHash.parse(response.data!) as XMLIndexer? {
+                    print(xml)
+                    let news = xml[Xml.news][Xml.item]
+                    let countNews = news.all.count
+//                    print(countNews)
+                    for i in 0..<countNews {
+                        
+                        guard let dateString = String("\((news[i][Xml.date].element?.text)!) \((news[i][Xml.time].element?.text)!)") as String? else {
+                            return
+                        }
+                        
+                        let timestamp = formatTime(dateString: dateString)
+                        let news = News(
+                            title:(news[i][Xml.title].element?.text)!,
+                            shortDescription: (news[i][Xml.shortdescription].element?.text)!,
+                            fullDescription: (news[i][Xml.description].element?.text)!,
+                            date: dateString, timestamp : timestamp,
+                            pictureUrl: (news[i][Xml.picture].element?.text)!,
+                            croppedPictureUrl: (news[i][Xml.croppedPicture].element?.text)!,
+                            link: (news[i][Xml.source].element?.text)!)
+                        globalNews.append(news)
+                        
+                    }
+                    globalNews.sort {
+                        return $0.timestamp > $1.timestamp
+                    }
+                    completionHandler(true)
+                } else {
+                    completionHandler(false)
+                }
+            } else {
+                completionHandler(false)
+                print("Error parse http", response.error!)
+            }
+        })
+    }
+    
+}
